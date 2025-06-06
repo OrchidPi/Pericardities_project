c0b6f65bf1304c20e098eee2659308c08f7567d7
Jialu Pi <165834545+OrchidPi@users.noreply.github.com>
2025-06-06 07:46:28 +0000

    main

--- pericarditis_app.py
+++ pericarditis_app.py
@@ -6,13 +6,23 @@ import os
 def ensure_package(package, pip_name=None):
     pip_name = pip_name or package
     if importlib.util.find_spec(package) is None:
+        print(f"🔧 Installing {pip_name} ...")
         subprocess.check_call([sys.executable, "-m", "pip", "install", pip_name])
-        os.execv(sys.executable, [sys.executable] + sys.argv)  # Restart to load
+        os.execv(sys.executable, [sys.executable] + sys.argv)  # Restart the script
 
-# Install required packages
+# List of packages
 ensure_package("joblib")
 ensure_package("torch")
-ensure_package("cv2", "opencv-python")
+ensure_package("cv2", "opencv-python")         # 'cv2' is provided by 'opencv-python'
+ensure_package("easydict")
+ensure_package("shap")
+ensure_package("matplotlib")
+ensure_package("gdown")
+ensure_package("streamlit_cropper", "streamlit-cropper")
+ensure_package("transformers")
+ensure_package("torchvision")
+ensure_package("nltk")
+
 
 import joblib
 import streamlit as st
